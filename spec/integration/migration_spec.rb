0a74902d0ddf2ef898366f271d258340a9cda917
Dan Kubb <dan.kubb@gmail.com>
2010-05-18 15:16:52 -0700

    Added guard clauses for in_memory and yaml adapters
    * While yaml has some limited support for migrations, it is not complete
      and in_memory has none. Future updates may include methods for
      these adapters, but for now migrations should be considered
      unsupported for these adapters.

--- spec/integration/migration_spec.rb
+++ spec/integration/migration_spec.rb
@@ -1,133 +1,138 @@
 require 'spec_helper'
 
-
 describe "A Migration" do
-  describe DataMapper::Migration, 'interface' do
 
-    before(:all) do
-      @adapter = DataMapper::Spec.adapter
-    end
+  supported_by :postgres, :mysql, :sqlite, :oracle, :sqlserver do
 
-    before do
-      @migration = DataMapper::Migration.new(1, :create_people_table, :verbose => false) { }
-    end
+    describe DataMapper::Migration, 'interface' do
 
-    it "should have a postition attribute" do
-      @migration.should respond_to(:position)
-      @migration.should respond_to(:position=)
-      @migration.position.should == 1
-    end
+      before(:all) do
+        @adapter = DataMapper::Spec.adapter
+      end
 
-    it "should have a name attribute" do
-      @migration.should respond_to(:name)
-      @migration.should respond_to(:name=)
-      @migration.name.should == :create_people_table
-    end
+      before do
+        @migration = DataMapper::Migration.new(1, :create_people_table, :verbose => false) { }
+      end
 
-    it "should have a :database option" do
-      adapter = DataMapper::Spec.adapter(:alternate)
-      m = DataMapper::Migration.new(2, :create_dogs_table, :database => :alternate) {}
-      m.instance_variable_get(:@adapter).should == adapter
-    end
+      it "should have a postition attribute" do
+        @migration.should respond_to(:position)
+        @migration.should respond_to(:position=)
+        @migration.position.should == 1
+      end
 
-    it "should use the default database by default" do
-      @migration.instance_variable_get(:@database).name.should == :default
-    end
+      it "should have a name attribute" do
+        @migration.should respond_to(:name)
+        @migration.should respond_to(:name=)
+        @migration.name.should == :create_people_table
+      end
 
-    it "should have a verbose option" do
-      m = DataMapper::Migration.new(2, :create_dogs_table, :verbose => false) {}
-      m.instance_variable_get(:@verbose).should == false
-    end
+      it "should have a :database option" do
+        adapter = DataMapper::Spec.adapter(:alternate)
+        m = DataMapper::Migration.new(2, :create_dogs_table, :database => :alternate) {}
+        m.instance_variable_get(:@adapter).should == adapter
+      end
 
-    it "should be verbose by default" do
-      m = DataMapper::Migration.new(2, :create_dogs_table) {}
-      m.instance_variable_get(:@verbose).should == true
-    end
+      it "should use the default database by default" do
+        @migration.instance_variable_get(:@database).name.should == :default
+      end
 
-    it "should be sortable, first by position, then name" do
-      m1 = DataMapper::Migration.new(1, :create_people_table) {}
-      m2 = DataMapper::Migration.new(2, :create_dogs_table) {}
-      m3 = DataMapper::Migration.new(2, :create_cats_table) {}
-      m4 = DataMapper::Migration.new(4, :create_birds_table) {}
+      it "should have a verbose option" do
+        m = DataMapper::Migration.new(2, :create_dogs_table, :verbose => false) {}
+        m.instance_variable_get(:@verbose).should == false
+      end
 
-      [m1, m2, m3, m4].sort.should == [m1, m3, m2, m4]
-    end
+      it "should be verbose by default" do
+        m = DataMapper::Migration.new(2, :create_dogs_table) {}
+        m.instance_variable_get(:@verbose).should == true
+      end
 
-    adapter = DataMapper::Spec.adapter_name
+      it "should be sortable, first by position, then name" do
+        m1 = DataMapper::Migration.new(1, :create_people_table) {}
+        m2 = DataMapper::Migration.new(2, :create_dogs_table) {}
+        m3 = DataMapper::Migration.new(2, :create_cats_table) {}
+        m4 = DataMapper::Migration.new(4, :create_birds_table) {}
 
-    expected_module_lambda = {
-      :sqlite   => lambda { SQL::Sqlite   },
-      :mysql    => lambda { SQL::Mysql    },
-      :postgres => lambda { SQL::Postgres }
-    }[adapter.to_sym]
+        [m1, m2, m3, m4].sort.should == [m1, m3, m2, m4]
+      end
 
-    expected_module = expected_module_lambda ? expected_module_lambda.call : nil
+      adapter = DataMapper::Spec.adapter_name
 
-    if expected_module
-      it "should extend with #{expected_module} when adapter is #{adapter}" do
-        migration = DataMapper::Migration.new(1, :"#{adapter}_adapter_test") { }
-        (class << migration.adapter; self; end).included_modules.should include(expected_module)
+      expected_module_lambda = {
+        :sqlite   => lambda { SQL::Sqlite   },
+        :mysql    => lambda { SQL::Mysql    },
+        :postgres => lambda { SQL::Postgres }
+      }[adapter.to_sym]
+
+      expected_module = expected_module_lambda ? expected_module_lambda.call : nil
+
+      if expected_module
+        it "should extend with #{expected_module} when adapter is #{adapter}" do
+          migration = DataMapper::Migration.new(1, :"#{adapter}_adapter_test") { }
+          (class << migration.adapter; self; end).included_modules.should include(expected_module)
+        end
       end
     end
+
+    describe DataMapper::Migration, 'defining actions' do
+      before do
+        @migration = DataMapper::Migration.new(1, :create_people_table, :verbose => false) { }
+      end
+
+      it "should have an #up method" do
+        @migration.should respond_to(:up)
+      end
+
+      it "should save the block passed into the #up method in @up_action" do
+        action = lambda {}
+        @migration.up(&action)
+
+        @migration.instance_variable_get(:@up_action).should == action
+      end
+
+      it "should have a #down method" do
+        @migration.should respond_to(:down)
+      end
+
+      it "should save the block passed into the #down method in @down_action" do
+        action = lambda {}
+        @migration.down(&action)
+
+        @migration.instance_variable_get(:@down_action).should == action
+      end
+
+      it "should make available an #execute method" do
+        @migration.should respond_to(:execute)
+      end
+
+      it "should run the sql passed into the #execute method"
+      # TODO: Find out how to stub the DataMapper::database.execute method
+    end
+
+    describe DataMapper::Migration, "output" do
+      before do
+        @migration = DataMapper::Migration.new(1, :create_people_table) { }
+        @migration.stub!(:write) # so that we don't actually write anything to the console!
+      end
+
+      it "should #say a string with an indent" do
+        @migration.should_receive(:write).with("   Foobar")
+        @migration.say("Foobar", 2)
+      end
+
+      it "should #say with a default indent of 4" do
+        @migration.should_receive(:write).with("     Foobar")
+        @migration.say("Foobar")
+      end
+
+      it "should #say_with_time the running time of a block" do
+        @migration.should_receive(:write).with(/Block/)
+        @migration.should_receive(:write).with(/-> [\d]+/)
+
+        @migration.say_with_time("Block"){ }
+      end
+
+    end
+
   end
 
-  describe DataMapper::Migration, 'defining actions' do
-    before do
-      @migration = DataMapper::Migration.new(1, :create_people_table, :verbose => false) { }
-    end
-
-    it "should have an #up method" do
-      @migration.should respond_to(:up)
-    end
-
-    it "should save the block passed into the #up method in @up_action" do
-      action = lambda {}
-      @migration.up(&action)
-
-      @migration.instance_variable_get(:@up_action).should == action
-    end
-
-    it "should have a #down method" do
-      @migration.should respond_to(:down)
-    end
-
-    it "should save the block passed into the #down method in @down_action" do
-      action = lambda {}
-      @migration.down(&action)
-
-      @migration.instance_variable_get(:@down_action).should == action
-    end
-
-    it "should make available an #execute method" do
-      @migration.should respond_to(:execute)
-    end
-
-    it "should run the sql passed into the #execute method"
-    # TODO: Find out how to stub the DataMapper::database.execute method
-  end
-
-  describe DataMapper::Migration, "output" do
-    before do
-      @migration = DataMapper::Migration.new(1, :create_people_table) { }
-      @migration.stub!(:write) # so that we don't actually write anything to the console!
-    end
-
-    it "should #say a string with an indent" do
-      @migration.should_receive(:write).with("   Foobar")
-      @migration.say("Foobar", 2)
-    end
-
-    it "should #say with a default indent of 4" do
-      @migration.should_receive(:write).with("     Foobar")
-      @migration.say("Foobar")
-    end
-
-    it "should #say_with_time the running time of a block" do
-      @migration.should_receive(:write).with(/Block/)
-      @migration.should_receive(:write).with(/-> [\d]+/)
-
-      @migration.say_with_time("Block"){ }
-    end
-
-  end
 end
--- spec/isolated/require_before_setup_spec.rb
+++ spec/isolated/require_before_setup_spec.rb
@@ -1,11 +1,13 @@
 require 'spec'
 require 'isolated/require_spec'
 require 'dm-core/spec/setup'
+require 'dm-core/spec/lib/adapter_helpers'
 
 # To really test this behavior, this spec needs to be run in isolation and not
 # as part of the typical rake spec run, which requires dm-transactions upfront
 
 describe "require 'dm-migrations' before calling DataMapper.setup" do
+  extend DataMapper::Spec::Adapters::Helpers
 
   before(:all) do
 
@@ -21,6 +23,8 @@ describe "require 'dm-migrations' before calling DataMapper.setup" do
 
   end
 
-  it_should_behave_like "require 'dm-migrations'"
+  supported_by :postgres, :mysql, :sqlite, :oracle, :sqlserver do
+    it_should_behave_like "require 'dm-migrations'"
+  end
 
 end
