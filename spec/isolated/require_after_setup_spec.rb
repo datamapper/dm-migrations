38619176a6e6b7a2c1c9afd461256e7971b33a35
Dan Kubb <dan.kubb@gmail.com>
2010-05-20 11:26:13 -0700

    Fixed failing specs

--- spec/isolated/require_after_setup_spec.rb
+++ spec/isolated/require_after_setup_spec.rb
@@ -1,30 +1,30 @@
 require 'spec'
 require 'isolated/require_spec'
 require 'dm-core/spec/setup'
-require 'dm-core/spec/lib/adapter_helpers'
 
 # To really test this behavior, this spec needs to be run in isolation and not
 # as part of the typical rake spec run, which requires dm-transactions upfront
 
-describe "require 'dm-migrations' after calling DataMapper.setup" do
-  extend DataMapper::Spec::Adapters::Helpers
+if %w[postgres mysql sqlite oracle sqlserver].include?(ENV['ADAPTER'])
 
-  before(:all) do
+  describe "require 'dm-migrations' after calling DataMapper.setup" do
 
-    @adapter = DataMapper::Spec.adapter
-    require 'dm-migrations'
+    before(:all) do
 
-    class ::Person
-      include DataMapper::Resource
-      property :id, Serial
+      @adapter = DataMapper::Spec.adapter
+      require 'dm-migrations'
+
+      class ::Person
+        include DataMapper::Resource
+        property :id, Serial
+      end
+
+      @model   = Person
+
     end
 
-    @model   = Person
-
-  end
-
-  supported_by :postgres, :mysql, :sqlite, :oracle, :sqlserver do
     it_should_behave_like "require 'dm-migrations'"
+
   end
 
 end
--- spec/isolated/require_before_setup_spec.rb
+++ spec/isolated/require_before_setup_spec.rb
@@ -1,30 +1,30 @@
 require 'spec'
 require 'isolated/require_spec'
 require 'dm-core/spec/setup'
-require 'dm-core/spec/lib/adapter_helpers'
 
 # To really test this behavior, this spec needs to be run in isolation and not
 # as part of the typical rake spec run, which requires dm-transactions upfront
 
-describe "require 'dm-migrations' before calling DataMapper.setup" do
-  extend DataMapper::Spec::Adapters::Helpers
+if %w[postgres mysql sqlite oracle sqlserver].include?(ENV['ADAPTER'])
 
-  before(:all) do
+  describe "require 'dm-migrations' before calling DataMapper.setup" do
 
-    require 'dm-migrations'
-    @adapter = DataMapper::Spec.adapter
+    before(:all) do
 
-    class ::Person
-      include DataMapper::Resource
-      property :id, Serial
+      require 'dm-migrations'
+      @adapter = DataMapper::Spec.adapter
+
+      class ::Person
+        include DataMapper::Resource
+        property :id, Serial
+      end
+
+      @model   = Person
+
     end
 
-    @model   = Person
-
-  end
-
-  supported_by :postgres, :mysql, :sqlite, :oracle, :sqlserver do
     it_should_behave_like "require 'dm-migrations'"
+
   end
 
 end
