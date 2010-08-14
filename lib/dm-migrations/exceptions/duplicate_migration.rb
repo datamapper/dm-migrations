19e1f328cade83937af24aef4c411a4cc0e4151f
Postmodern <postmodern.mod3@gmail.com>
2010-08-13 22:04:43 -0700

    Do not override StandardError#initialize in DuplicateMigration.
    * Other libraries might want to use different messages.

--- lib/dm-migrations/exceptions/duplicate_migration.rb
+++ lib/dm-migrations/exceptions/duplicate_migration.rb
@@ -1,11 +1,6 @@
 module DataMapper
   module Migrations
     class DuplicateMigration < StandardError
-
-      def initialize(migration)
-        super("Duplicate Migration Name: '#{migration.name}', version: #{migration.position}")
-      end
-
     end
   end
 end
