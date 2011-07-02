3409e9f9c3dc121b68a4b8241b8b55de102e816b
Jordan Ritter <jpr5@darkridge.com>
2011-07-01 23:22:48 -0700

    Respect repository scope when specified.
    Previously, given configured repositories :a and :b, auto_{upgrade,migrate}!(:a)
    would affect :b also.

--- lib/dm-migrations/auto_migration.rb
+++ lib/dm-migrations/auto_migration.rb
@@ -41,8 +41,10 @@ module DataMapper
 
       # @api private
       def repository_execute(method, repository_name)
-        DataMapper::Model.descendants.each do |model|
-          model.send(method, repository_name || model.default_repository_name)
+        models = DataMapper::Model.descendants
+        models = models.select { |m| m.default_repository_name == repository_name } if repository_name
+        models.each do |model|
+          model.send(method, model.default_repository_name)
         end
       end
     end
