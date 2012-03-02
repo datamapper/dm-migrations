a77f7fe686a5e8403e12fd4440b6d7a93fa4b037
Patrick Cheng <patrickyccheng@gmail.com>
2012-03-02 11:04:42 -0800

    added change_column_type_statement method for both oracle and sqlserver

--- lib/dm-migrations/sql/oracle.rb
+++ lib/dm-migrations/sql/oracle.rb
@@ -2,5 +2,10 @@ require 'dm-migrations/sql/table'
 
 module SQL
   module Oracle
+
+    def change_column_type_statement(name, column)
+      "ALTER TABLE #{quote_name(name)} MODIFY ( #{column.to_sql} )"
+    end
+
   end
 end
--- lib/dm-migrations/sql/sqlserver.rb
+++ lib/dm-migrations/sql/sqlserver.rb
@@ -2,5 +2,10 @@ require 'dm-migrations/sql/table'
 
 module SQL
   module Sqlserver
+
+    def change_column_type_statement(name, column)
+      "ALTER TABLE #{quote_name(name)} ALTER COLUMN #{column.to_sql}"
+    end
+    
   end
 end
