d46e5e853b74702b5101489743d29a4f03e92007
Andrés Villagrán <andres@villagranquiroz.cl>
2015-03-19 13:54:02 -0300

    Update table_modifier.rb

--- lib/dm-migrations/sql/table_modifier.rb
+++ lib/dm-migrations/sql/table_modifier.rb
@@ -19,8 +19,8 @@ module SQL
       @statements << "ALTER TABLE #{quoted_table_name} ADD COLUMN #{column.to_sql}"
     end
 
-    def foreign_key_exists?(column, reference, reference_id = 'id')
-      execute "SELECT TRUE FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_TYPE = 'FOREIGN KEY' AND TABLE_SCHEMA = #{quoted_table_name} AND CONSTRAINT_NAME = '#{quote_column_name(@table_name+'_'+reference.to_s.gsub('_id', '')+'_fk')}'"    
+    def foreign_key_exists?(constraint_name)
+      execute "SELECT TRUE FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_TYPE = 'FOREIGN KEY' AND TABLE_SCHEMA = #{quoted_table_name} AND CONSTRAINT_NAME = '#{quote_column_name(@table_name+'_'+constraint_name.to_s.gsub('_id', '')+'_fk')}'"    
     end
 
     def add_foreign_key(column, reference, reference_id = 'id')
